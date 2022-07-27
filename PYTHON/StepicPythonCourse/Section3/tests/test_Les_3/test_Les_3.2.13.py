bc8f370bc3372dadbbd9d37dbb1e362d999c4cab
Andrew Shabailov <99425041+Andrewsha92@users.noreply.github.com>
2022-07-28 02:31:35 +0300

    new tests are added

---
+++ PYTHON/StepicPythonCourse/Section3/tests/test_Les_3/test_Les_3.2.13.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import unittest
+
+
+class TestUnit(unittest.TestCase):
+
+    def test_registration_1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        elements = browser.find_elements(By.TAG_NAME, "input")
+        for element in elements:
+            element.send_keys("Мой ответ")
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+    def test_registration_2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1_FIRST_NAME = browser.find_element(By.XPATH, "//form/div[1]/div[1]/input")
+        input1_FIRST_NAME.send_keys("Ivan Shabailov")
+        time.sleep(3)
+
+        input2_EMAIL = browser.find_element(By.CSS_SELECTOR, "div.first_block > div.form-group.second_class > input")
+        input2_EMAIL.send_keys("PASS")
+
+        input3_PHONE = browser.find_element(By.XPATH, "//form/div[2]/div[1]/input")
+        input3_PHONE.send_keys("Smolensk")
+
+        input4_ADDRES = browser.find_element(By.XPATH, "//form/div[2]/div[2]/input")
+        input4_ADDRES.send_keys("Russia")
+        button = browser.find_element(By.XPATH, "//div/form/button")
+        button.click()
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+
+if __name__ == "__main__":
+    unittest.main()
