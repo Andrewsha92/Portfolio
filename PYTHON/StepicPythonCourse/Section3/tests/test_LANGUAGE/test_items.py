17b6c430390636e046852399d120631e1d8b900d
Andrew Shabailov <99425041+Andrewsha92@users.noreply.github.com>
2022-07-31 16:28:52 +0300

    file is changed

--- PYTHON/StepicPythonCourse/Section3/tests/test_LANGUAGE/test_items.py
+++ PYTHON/StepicPythonCourse/Section3/tests/test_LANGUAGE/test_items.py
@@ -9,6 +9,6 @@ def test_button_add_with_difference_language_on_page(browser):
     assert browser.find_element(By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]'), \
         "The page does not have an 'ADD to basket' button!"
 
-# For starting test you need to use command: pytest -s -v --language=ru test_items.py
+# For starting test you need to use command: pytest -s -v --language=ru test_main_page.py
 # Where param language you choose by yourself
 # You can choose: ar, ca, cs, da, de, en-gb, el, es, fi, fr, it, ko. nl, pl, pt, pt-br, ro, ru, sk, uk, zh-hans.
