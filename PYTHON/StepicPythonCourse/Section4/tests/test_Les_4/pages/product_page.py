d1e69c1fd9a79065a9ba7279cf1f761901765638
Andrew Shabailov <99425041+Andrewsha92@users.noreply.github.com>
2022-08-02 17:43:58 +0300

    product page is modified

--- PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/pages/product_page.py
+++ PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/pages/product_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
-from selenium.webdriver.common.by import By
 
 
 class ProductPage(BasePage):
@@ -8,6 +7,9 @@ class ProductPage(BasePage):
     def should_be_button_on_the_page(self):
         self.guest_can_add_product_to_basket()
         self.button_add_click()
+        self.guest_can_add_product_to_basket()
+        self.checking_name_ordered_book_and_book_in_basket()
+        self.checking_goods_price_and_basket_price_are_the_same()
 
     def button_add_click(self):
         button_add = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
@@ -16,9 +18,6 @@ class ProductPage(BasePage):
     def guest_can_add_product_to_basket(self):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET), \
             "Button 'add to bag' is not presented"
-
-    def guest_can_see_promo(self):
-        pass
 
     def checking_name_ordered_book_and_book_in_basket(self):
         ordered_book = self.browser.find_element(*ProductPageLocators.ORDERED_BOOK).text
@@ -30,6 +29,5 @@ class ProductPage(BasePage):
     def checking_goods_price_and_basket_price_are_the_same(self):
         price_of_book = self.browser.find_element(*ProductPageLocators.PRICE_OF_BOOK).text
         price_of_basket = self.browser.find_element(*ProductPageLocators.PRICE_OF_BASKET).text
+        print(price_of_book, price_of_basket)
         assert price_of_book == price_of_basket, 'The price of book and the price of basket are not the same'
-
-
