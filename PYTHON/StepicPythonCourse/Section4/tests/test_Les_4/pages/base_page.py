421193a9310596d57927fb4cbbbdd47af70b6023
Andrew Shabailov <99425041+Andrewsha92@users.noreply.github.com>
2022-08-02 19:43:56 +0300

    added methods

--- PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/pages/base_page.py
+++ PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/pages/base_page.py
@@ -1,5 +1,7 @@
-from selenium.common.exceptions import NoAlertPresentException  # в начале файла
+from selenium.common.exceptions import NoAlertPresentException, TimeoutException  # в начале файла
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import math
 
 
@@ -23,6 +25,24 @@ class BasePage():
             return False
         return True
 
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout). \
+                until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
@@ -36,6 +56,3 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
-
-
-
