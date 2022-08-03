e824247c31d3fa1f1f79de56aff529c431ecc9dd
Andrew Shabailov <99425041+Andrewsha92@users.noreply.github.com>
2022-08-03 21:06:00 +0300

    test of basket is added

--- PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/test_basket_page.py
+++ PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/test_basket_page.py
@@ -1,13 +1,19 @@
 from .pages.basket_page import BasketPage
 
-link = "http://selenium1py.pythonanywhere.com/"
-
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
     page = BasketPage(browser, link)
     page.open()
-    page.go_to_login_page()
     page.click_on_basket()
     page.the_basket_is_empty()
     page.price_of_basket_is_not_present()
 
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.click_on_basket()
+    page.the_basket_is_empty()
+    page.price_of_basket_is_not_present()
