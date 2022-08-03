aedb331cd0ef98251aef34c856f66e61ca8b5496
Andrew Shabailov <99425041+Andrewsha92@users.noreply.github.com>
2022-08-03 18:52:42 +0300

    file is modified

--- PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/test_product_page.py
+++ PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/test_product_page.py
@@ -29,3 +29,17 @@ def test_checking_name_ordered_book_and_book_in_basket(browser):
     page.button_add_click()  # кликаем на кнопку добавить
     page.solve_quiz_and_get_code()
     page.checking_name_ordered_book_and_book_in_basket()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
