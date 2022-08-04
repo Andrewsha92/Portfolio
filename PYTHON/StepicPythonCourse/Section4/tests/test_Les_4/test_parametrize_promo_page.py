d81dc5f922066fcbd21fc2f4c5379f61f5d7c207
Andrew Shabailov <99425041+Andrewsha92@users.noreply.github.com>
2022-08-04 21:29:47 +0300

    marked failed test

--- PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/test_parametrize_promo_page.py
+++ PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/test_parametrize_promo_page.py
@@ -6,8 +6,8 @@ from .pages.promo_page import PromoPage
                          [pytest.param(i, marks=pytest.mark.xfail(i == 7, reason='Failed test')) for i in range(10)])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
-    page = PromoPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
+    page = PromoPage(browser, link)
+    page.open()
     page.button_add_click()
     page.solve_quiz_and_get_code()
     page.checking_goods_price_and_basket_price_are_the_same()
