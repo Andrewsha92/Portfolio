369c848bc2a8bb5b2d75d6f6d4b918eeeb3c08be
Andrew Shabailov <99425041+Andrewsha92@users.noreply.github.com>
2022-08-04 19:26:35 +0300

    deleted comments

--- PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/test_login_page.py
+++ PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/test_login_page.py
@@ -1,22 +1,21 @@
 from .pages.login_page import LoginPage
 
+link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+
 
 def test_sub_login_in_url(browser):
-    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.should_be_login_url()  # выполняем метод страницы — переходим на страницу логина
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
 
 
 def test_login_form_on_the_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.should_be_login_form()  # выполняем метод страницы — переходим на страницу логина
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
 
 
 def test_register_form_on_the_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_register_form()
