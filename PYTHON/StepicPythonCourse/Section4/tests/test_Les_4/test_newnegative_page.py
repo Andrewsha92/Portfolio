a813e060c254ae17a56eabe4bd2fdfd22835dc54
Andrew Shabailov <99425041+Andrewsha92@users.noreply.github.com>
2022-08-04 21:32:09 +0300

    negative tests are added

--- PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/test_newnegative_page.py
+++ PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/test_newnegative_page.py
@@ -4,23 +4,19 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
 
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    page = NewPage(browser,
-                   link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.button_add_click()  # кликаем на кнопку добавить
-    page.should_not_be_success_message_is_not_pres()
+    page = NewPage(browser, link)
+    page.open()
+    page.button_add_click()
+    page.should_not_be_success_message_is_not_present()
 
 
 def test_guest_cant_see_success_message(browser):
-    page = NewPage(browser,
-                   link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.should_not_be_success_message_is_not_pres()
+    page = NewPage(browser, link)
+    page.should_not_be_success_message_is_not_present()
 
 
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    page = NewPage(browser,
-                   link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.button_add_click()  # кликаем на кнопку добавить
+    page = NewPage(browser, link)
+    page.open()
+    page.button_add_click()
     page.should_not_be_success_message_is_disappeared()
