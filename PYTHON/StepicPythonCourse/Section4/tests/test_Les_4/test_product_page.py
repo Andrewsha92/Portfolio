334207d7b6f6a0d7a309cdf24451676de008840e
Andrew Shabailov <99425041+Andrewsha92@users.noreply.github.com>
2022-08-04 19:18:22 +0300

    added fixture and new class

--- PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/test_product_page.py
+++ PYTHON/StepicPythonCourse/Section4/tests/test_Les_4/test_product_page.py
@@ -1,33 +1,52 @@
+import time
+import pytest
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser,
-                       link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.button_add_click()  # кликаем на кнопку добавить
-    page.solve_quiz_and_get_code()
-    page.guest_can_add_product_to_basket()  # выполняем метод страницы — переходим на страницу логина
+@pytest.mark.login
+class TestUserAddToBasketFromProductPage:
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        email = str(time.time()) + '@fakemail.org'
+        password = 'atatatata'
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message_is_not_present()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.button_add_click()
+        page.solve_quiz_and_get_code()
+        page.guest_can_add_product_to_basket()
 
 
 def test_checking_goods_price_and_basket_price_are_the_same(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser,
-                       link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.button_add_click()  # кликаем на кнопку добавить
+    page = ProductPage(browser, link)
+    page.open()
+    page.button_add_click()
     page.solve_quiz_and_get_code()
     page.checking_goods_price_and_basket_price_are_the_same()
 
 
 def test_checking_name_ordered_book_and_book_in_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser,
-                       link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.button_add_click()  # кликаем на кнопку добавить
+    page = ProductPage(browser, link)
+    page.open()
+    page.button_add_click()
     page.solve_quiz_and_get_code()
     page.checking_name_ordered_book_and_book_in_basket()
 
@@ -44,6 +63,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
