490bcc951343d9081666c3a7f0a7f474eaf87f6b
Andrew Shabailov <99425041+Andrewsha92@users.noreply.github.com>
2022-08-09 17:49:04 +0300

    added new page

--- PYTHON/OZON_automation/pages/parametrize_page.py
+++ PYTHON/OZON_automation/pages/parametrize_page.py
@@ -11,8 +11,3 @@ class TestParametrizePage(BasePage):
         self.browser.find_element(*ParametrizePageLocators.SEARCH_STRING).send_keys(string)
         self.browser.find_element(*ParametrizePageLocators.SEARCH_BUTTON).click()
         assert self.is_element_present(*ParametrizePageLocators.SUCCESS_MESSAGE), 'Success message is not presented'
-
-
-
-
-
